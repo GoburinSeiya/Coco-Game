a46423092ee881e10b77b69e598f3d7c42591733
Rishardo2003 <47266593+Rishardo2003@users.noreply.github.com>
2023-07-08 13:55:31 -0700

    deteccion de proximidad ui

--- Assets/Scripts/Player/SphereDetector.cs
+++ Assets/Scripts/Player/SphereDetector.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class SphereDetector : MonoBehaviour
 {
+    public string tagDetect = "PopUpElements";
+    public string childNameUI = "UIobject";
     public float radius = 3f; // Radio de la esfera de detección
-    public GameObject[] objectArray;
-
+    //public GameObject otherObject;
     private void Update()
     {
 
@@ -14,9 +15,53 @@ public class SphereDetector : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 3)
+        GameObject otherObject = other.gameObject;
+        Tags otherTags = other.GetComponent<Tags>();
+
+        //Comprueba que exista el script
+        if (otherTags != null)
         {
-            Debug.Log(other.gameObject.layer);
+            //Recorre el array de tags del objeto
+            foreach (var tag in otherTags.tags)
+            {
+                //Cuando el tag coincide con el que se busca
+                if(tagDetect == tag)
+                {
+                    Transform child = otherObject.transform.Find("UIobject");
+                    child.gameObject.SetActive(true);
+                    Debug.Log(child.name);
+                }
+            }
+        }
+        else 
+        {
+            Debug.Log("No hay");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject otherObject = other.gameObject;
+        Tags otherTags = other.GetComponent<Tags>();
+
+        //Comprueba que exista el script
+        if (otherTags != null)
+        {
+            //Recorre el array de tags del objeto
+            foreach (var tag in otherTags.tags)
+            {
+                //Cuando el tag coincide con el que se busca
+                if (tagDetect == tag)
+                {
+                    Transform child = otherObject.transform.Find("UIobject");
+                    child.gameObject.SetActive(false);
+                    Debug.Log(child.name);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("No hay");
         }
     }
 }
