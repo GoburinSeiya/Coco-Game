a8428e9c734e73935c3d040ab45400d18acbbfff
ProReaps <angeldejesussandovalbrito@gmail.com>
2023-07-24 20:40:09 -0700

    Antorcha con modelo nuevo y particulas / plataforma que se mueve cuando se activa
    pipipipipi

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -6,19 +6,22 @@ public class Torch : MonoBehaviour, IDamageable
 {
     private Transform tr;
     GameObject cube;
+    GameObject Flame_Prefab;
+    GameObject Flame;
     public bool lit = false;
     void Start()
     {
         tr = GetComponent<Transform>();
+        Flame_Prefab = Resources.Load("Prefabs/Parent_Fire", typeof(GameObject)) as GameObject;
     }
     public void TakeDamage(int _value) 
     {
         if (!lit)
         {
-            cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.SetParent(this.gameObject.transform);
-            cube.transform.localScale = new Vector3(0.75f, 0.3525f, 0.75f);
-            cube.transform.position = tr.position + new Vector3(0.0f, 0.6f, 0.0f);
+            Flame = GameObject.Instantiate(Flame_Prefab);
+            Flame.transform.SetParent(this.gameObject.transform);
+            Flame.transform.localScale = new Vector3(0.75f, 0.3525f, 0.75f);
+            Flame.transform.position = tr.position + new Vector3(0.0f, 0.6f, 0.0f);
             lit = true;
         }
     }
